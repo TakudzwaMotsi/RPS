f6584c7ab862cd3801a3e981ab281a5acc1c02a3
TakudzwaMotsi <takumotsi@gmail.com>
2024-11-25 23:48:51 +0200

    added game loop

--- rps.js
+++ rps.js
@@ -64,4 +64,38 @@ function playRound(humanChoice, computerChoice){
 
 
 }
+function playGame() {
+   
+    for(let i=0; i<5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+    }
+
+    if(humanScore>computerScore){
+        alert("your total score is: " + humanScore + "\nComputer total score is: " + computerScore + "\nyou win " );
+
+    }
+    else if(computerScore>humanScore){
+        alert("your total score is: " + humanScore + "\nComputer total score is: " + computerScore + "\nyou lose " );
+
+    }
+    else{
+        alert("your total score is: " + humanScore + "\nComputer total score is: " + computerScore + "\nits a draw " );
+
+    }
+
+i=0
+humanScore=0
+computerScore=0
+    // Ask if the user wants to play again
+    let playAgain = confirm("Do you want to play again?");
+    if (playAgain) {
+        playGame(); // recursive call to play again
+    }
+}
+
+// Start the game
+playGame();
 
