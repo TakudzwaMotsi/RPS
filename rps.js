87c40be278c88c6f1834b968279fdcf29e4d2e16
TakudzwaMotsi <takumotsi@gmail.com>
2024-11-25 18:06:16 +0200

    computer function returns randomly rock, paper or scissors

--- rps.js
+++ rps.js
@@ -1,7 +1,18 @@
 function getComputerChoice(){
     let choice 
     let number = Math.round(Math.random()*2)
-    console.log(number)
+   
+    if(number==0){
+        choice="rock"
+    }
+    else if(number==1){
+        choice="paper"
+    }
+    else{
+        choice="scissors"
+    }
+    return choice
+    console.log(choice)
 }
 
 getComputerChoice()
