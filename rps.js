c722ce0ebed31c488785b104e31203e3cebd2a75
TakudzwaMotsi <takumotsi@gmail.com>
2024-11-25 19:00:32 +0200

    added function for human choices

--- rps.js
+++ rps.js
@@ -15,4 +15,29 @@ function getComputerChoice(){
     console.log(choice)
 }
 
+function getHumanChoice(){
+    let option= prompt("1.rock 2.paper 3.scissors, select number for choice", 1)
+    let theChoice
+    if(option==1){
+        theChoice="rock"}
+        else if(option==2){
+            theChoice="paper"
+        }
+        else if(option==3){
+            theChoice="scissors"
+        }
+        else{
+            alert("enter a valid choice")
+        }
+    
+    alert("your choice is " + theChoice)
+
+    return theChoice
+}
+
+let humanScore = 0
+let computerScore = 0
+
+
+getHumanChoice()
 getComputerChoice()
